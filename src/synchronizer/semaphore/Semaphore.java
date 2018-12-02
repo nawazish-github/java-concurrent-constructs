350222f34387218d646b35f02dfa06f5e6622f9f
Mohammad Nawazish Khan <md.nawazish.khan@gmail.com>
2018-12-02 23:52:45 +0530

    Add tryAcquire() and timed tryAcquire() methods

--- src/synchronizer/semaphore/Semaphore.java
+++ src/synchronizer/semaphore/Semaphore.java
@@ -1,5 +1,7 @@
 package synchronizer.semaphore;
 
+import java.util.concurrent.TimeUnit;
+
 public class Semaphore {
     private int tokens;
 
@@ -53,6 +55,31 @@ public class Semaphore {
                 } catch (InterruptedException e) {}
             }
 
+        }
+    }
+
+    public boolean tryAcquire(){
+        synchronized (this){
+           return checkAndDispenceToken();
+        }
+    }
+
+    public boolean tryAcquire(long timeout,
+                              TimeUnit unit)
+            throws InterruptedException{
+
+        if (Thread.currentThread().isInterrupted())
+            throw new InterruptedException();
+
+        synchronized (this){
+            boolean status = checkAndDispenceToken();
+                if (!status){
+                    long expiryTime =  unit.toMillis(timeout);
+                    this.wait(expiryTime);
+                    status = checkAndDispenceToken();
+                    return status;
+                }else
+                    return status;
         }
     }
 }
